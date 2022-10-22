5ea1282fa57b3359d4ea4b50d074f49875a6fbcd
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-10-22 20:39:16 +0900

    テスト段階

---
+++ kadai2/main.js
@@ -0,0 +1,31 @@
+
+
+let secondsTimeCount = document.getElementById("secondsCounter");
+function pushStartButton(){
+secondsTimeCount.innerHTML = "1"
+}
+
+
+function pushStopButton(){
+  alert("ストップボタン")  
+}
+
+function pushResetButton(){
+  alert("リセットボタン")  
+}
+
+/*let count = 0
+const secondsCountUp = 
+  console.log(count++);
+  setInterval(secondsCountUp,1000)*/
+
+
+
+/*function pushStartButton(){
+log
+}
+/*  setInterval(log,1000)
+}
+
+
+*/
