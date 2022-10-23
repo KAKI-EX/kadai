8a8f90d015e6eb49feb5b2708f66a9f25d073ec6
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-10-24 00:22:59 +0900

    最後のhourがうまくいかない

--- kadai2/main.js
+++ kadai2/main.js
@@ -4,15 +4,34 @@ document.getElementById("secondsCounter").innerHTML = 0;
 document.getElementById("minutesCounter").innerHTML = 0;
 document.getElementById("hoursCounter").innerHTML = 0;
 
-let count= 0; //0からスタート
-let subSecondsTimeCount = function(){document.getElementById("subSecondsCounter").innerHTML = count++};
+let subSecondsCount = 0; //0からスタート
+let subSecondsTimeCount = function(){document.getElementById("subSecondsCounter").innerHTML = subSecondsCount++};
 let booleanSubSecond = true;  //booleanの初期値はtrue
+
+let secondsCount = 55;
+let secondsTimeCount = function(){document.getElementById("secondsCounter").innerHTML = secondsCount++};
+
+let minutesCount = 59;
+let minutesTimeCount = function(){document.getElementById("minutesCounter").innerHTML = minutesCount++};
+
+let hoursCount = 1;
+let hoursTimeCount = function(){document.getElementById("hoursCounter").innerHTML = hoursCount++};
 
 function pushStartButton(){
   if(booleanSubSecond == true){
     booleanSubSecond = false;  //booleanにfalseを再代入
     startSubSeconds = setInterval(function(){subSecondsTimeCount();
-      if(count>9){count = 0}} ,100); //計測開始とカウント9で0に戻る
+      if(subSecondsCount>9){ //サブセコンドが9以上になったら0に戻り1秒足す。
+        subSecondsCount = 0;
+        secondsTimeCount()}
+          if(secondsCount>59 && subSecondsCount==9){  //サブセコンド9、秒数59になったら0に戻り1分足す。
+            secondsCount = 0;
+            minutesTimeCount()}
+            if(minutesCount>59 && secondsCount>59){
+              minutesCount = 0;
+              hoursTimeCount()
+          }}
+        ,100); //計測開始とカウント9で0に戻る
   }else if(booleanSubSecond == false){  //falseの場合には何もしない(二度押し防止)
   }
 };
