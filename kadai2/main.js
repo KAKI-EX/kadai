67d25adfc9f2b4bee8d2f7dc450cbaf75edbfe4f
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-10-23 12:07:53 +0900

    ストップボタン実装

--- kadai2/main.js
+++ kadai2/main.js
@@ -3,13 +3,14 @@
 let count= 0;
 let secondsTimeCount = function(){document.getElementById("secondsCounter").innerHTML = count++};
 
+
 function pushStartButton(){
-setInterval(secondsTimeCount,1000)
+  startSeconds = setInterval(secondsTimeCount,1000);
 }
 
 
 function pushStopButton(){
-  alert("ストップボタン")  
+  clearInterval(startSeconds);
 }
 
 function pushResetButton(){
