5b4b2ea29ebc506bc4b341def414788eb8821ad4
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-10-23 18:15:22 +0900

    秒の二度押し禁止追加

--- kadai2/main.js
+++ kadai2/main.js
@@ -2,15 +2,23 @@
 
 let count= 0;
 let secondsTimeCount = function(){document.getElementById("secondsCounter").innerHTML = count++};
-
+let booleanSecond = true; //booleanの初期値はtrue
 
 function pushStartButton(){
-  startSeconds = setInterval(secondsTimeCount,1000);
+  if(booleanSecond == true){
+    booleanSecond = false;  //booleanにfalseを再代入
+    startSeconds = setInterval(secondsTimeCount,1000);  //計測開始
+  }else if(booleanSecond == false){  //falseの場合には何もしない(二度押し防止)
+  }
 }
 
-
 function pushStopButton(){
-  clearInterval(startSeconds);
+  if(booleanSecond == false){
+    clearInterval(startSeconds);
+    booleanSecond = true
+  }else if(booleanSecond == true){
+    alert("何もしない")
+  }
 }
 
 function pushResetButton(){
