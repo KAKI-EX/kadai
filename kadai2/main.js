54940d89fb14899781f41e1ecd31c8e9e34fefad
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-10-24 14:52:40 +0900

    論理演算式が意図した通りに動作しない

--- kadai2/main.js
+++ kadai2/main.js
@@ -5,26 +5,71 @@ document.getElementById("minutesCounter").innerHTML = 0;
 document.getElementById("hoursCounter").innerHTML = 0;
 
 let subSecondsCount = 0; //0からスタート
-let subSecondsTimeCount = function(){document.getElementById("subSecondsCounter").innerHTML = subSecondsCount++};
-let booleanSubSecond = true;  //booleanの初期値はtrue
+let subSeconds_TimeCount = document.getElementById("subSecondsCounter");
 
-let secondsCount = 55;
-let secondsTimeCount = function(){document.getElementById("secondsCounter").innerHTML = secondsCount++};
+let secondsCount = 0;
+let seconds_TimeCount = document.getElementById("secondsCounter");
 
-let minutesCount = 58;
-let minutesTimeCount = function(){document.getElementById("minutesCounter").innerHTML = minutesCount++};
+let minutesCount = 0;
+let minutes_TimeCount = document.getElementById("minutesCounter");
 
-let hoursCount = 1;
-let hoursTimeCount = function(){document.getElementById("hoursCounter").innerHTML = hoursCount++};
+let hoursCount = 0;
+let hours_TimeCount = document.getElementById("hoursCounter");
+
+function time_CountUp()
+{
+subSecondsCount += 1;
+subSeconds_TimeCount.innerHTML = subSecondsCount;
+seconds_TimeCount.innerHTML = secondsCount;
+
+  if(subSecondsCount==9){
+    subSecondsCount=-1;
+    secondsCount = secondsCount+=1;
+  }
+  if(secondsCount>59){
+    secondsCount=0;
+    minutesCount +=1;
+    minutes_TimeCount.innerHTML = minutesCount
+  }
+  if(minutesCount>59){
+    minutesCount=0;
+    hoursCount +=1;
+    hours_TimeCount.innerHTML = hoursCount
+  }
+}
 
 function pushStartButton(){
+  test = setInterval(function(){time_CountUp()},100)}
+
+
+
+
+
+/*
+let secondsCount = 0;
+let secondsTimeCount = document.getElementById("secondsCounter").innerHTML;
+
+let minutesCount = 0;
+let minutesTimeCount = document.getElementById("minutesCounter").innerHTML;
+
+let hoursCount = 1;
+let hoursTimeCount = document.getElementById("hoursCounter").innerHTML;*/
+
+
+
+
+/*function pushStartButton(){
+  startSubSeconds = setInterval("subSeconds_TimeCount()",1000)
+}
+
+/*function pushStartButton(){
   if(booleanSubSecond == true){
     booleanSubSecond = false;  //booleanにfalseを再代入
     startSubSeconds = setInterval(function(){subSecondsTimeCount();
-      if(subSecondsCount>9){ //サブセコンドが9以上になったら0に戻り1秒足す。
+      if(subSecondsCount>8){ //サブセコンドが9以上になったら0に戻り1秒足す。
         subSecondsCount = 0;
         secondsTimeCount()}
-          if(secondsCount>59 && subSecondsCount==9){  //サブセコンド9、秒数59になったら0に戻り1分足す。
+          if(secondsCount>9){  //サブセコンド9、秒数59になったら0に戻り1分足す。
             secondsCount = 0;
             minutesTimeCount()}
             if(minutesCount>59 && secondsCount==59 && subSecondsCount==9){
@@ -51,6 +96,4 @@ function pushResetButton(){
   let = document.getElementById("minutesCounter").innerHTML = 0;
   let = document.getElementById("hoursCounter").innerHTML = 0;
   let = count = 0
-};
-
-if(count>9){alert("成功！")};
+};*/
