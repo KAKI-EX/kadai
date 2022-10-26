9ad509e638214f717bbdf0bb5a2fb3af584a1dc6
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-10-26 18:31:35 +0900

    基本機能完成

--- kadai2_javascript/2.calculator/main.js
+++ kadai2_javascript/2.calculator/main.js
@@ -1,32 +1,43 @@
 
 
+
+
+calStatus = "Wait"; /* 待機状態 */
+let count = 0
+
 function buttonClick(target) {
-  let result = document.getElementById("result");
-  let target_value = target.innerHTML; 
-  let remove0 = result.innerHTML
-  let remove = remove0.slice(1);
+  target_value = target.innerHTML; 
+  result = document.getElementById("result");
+
+  calStatus = "Start" /* 数字が押されて計算スタート*/
 
   if(target_value == "AC"){
+      calStatus = "Stop"
+      calStatus = 0
       result.innerHTML = "0";
     }else if(target_value == "="){
-      booleanFlag = false
       result.innerHTML = eval(result.innerHTML);
 
     }else if(result.innerHTML == "0" && target_value !== "."){
       result.innerHTML =target_value;
-    
-/*    }else if(target_value = "0"){
-      booleanFlag = true;
-      result.innerHTML =target_value;
-
-    }else if(result.innerHTML == "0" && booleanFlag == false){
-      console.log("成功")
-      result.innerHTML = remove + target_value;*/
 
     }else{
         result.innerHTML += target_value
+        count = 0
     }
   }
+
+function buttonClick2(target2) {
+  target2_value = target2.innerHTML;
+  console.log(calStatus);
+  count++;
+
+  if(calStatus == "Wait"){  /* ステータスがStartになるまで入力しない、最初に記号を押したら何も返さない*/
+
+  }else if(calStatus == "Start" && count == 1){ /* 何か数字が入力され、かつ1回目の記号入力ならそのまま計算*/
+    result.innerHTML += target2_value 
+  }else if(calStatus == "Start" && count > 1){ /* 2回目の記号なら何も返さない*/
     
+  }
 
-
+}
