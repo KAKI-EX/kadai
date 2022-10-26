0ac9fd094b6c3c0ff8c48c5bcc4f821d85a7ee9f
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-10-26 20:47:45 +0900

    起業直後のダブル0に加えて、シングル0が追加可能で、小数点が押された場合の計算エラー回避実装

--- kadai2_javascript/2.calculator/main.js
+++ kadai2_javascript/2.calculator/main.js
@@ -4,7 +4,9 @@
 
 calStatus = "Wait"; /* 待機状態 */
 let count = 0
+let zeroCount = 0
 let w0Count = 0
+
 
 function buttonClick(target) {
   target_value = target.innerHTML; 
@@ -15,19 +17,24 @@ function buttonClick(target) {
   if(target_value == "AC"){
       calStatus = "Stop"
       count = 0
+      zeroCount = 0
       w0Count = 0
       result.innerHTML = "0";
     }else if(target_value == "="){
       result.innerHTML = eval(result.innerHTML);
       count = 0
+      zeroCount = 0
       w0Count = 0
 
     }else if(result.innerHTML == "0" && target_value !== "."){
       result.innerHTML = target_value;
       count = 0;
+      zeroCount = 0
       w0Count = 0;
+
     }else{
       count = 0;
+      zeroCount = 0
       w0Count = 0;
       calStatus = "Start"
       result.innerHTML += target_value
@@ -44,6 +51,8 @@ function buttonClick2(target2) {
 
   }else if(calStatus == "Start" && count == 1){ /* 何か数字が入力され、かつ1回目の記号入力ならそのまま計算*/
     result.innerHTML += target2_value 
+    zeroCount = 0
+    w0Count = 0;
   }else if(calStatus == "Start" && count > 1){ /* 2回目の記号なら何も返さない*/
     
   }
@@ -60,9 +69,21 @@ function buttonClick3(target3) {
   }else if(count >=1 && w0Count == 1){
     result.innerHTML += "0"
   }else if(count >=1 && w0Count >1 ){
-    console.log("記号の後に00が2回押された")
     
   }else{
     result.innerHTML += target3_value;
+  }
+}
+
+function buttonClick4(target4){
+  zeroCount++
+  target4_value = target4.innerHTML
+  console.log("ゼロカウント"+zeroCount)
+  if(count >=1 && zeroCount>1){
+
+  }else if(count >=1 && w0Count >=1){
+
+  }else{
+    result.innerHTML += target4_value
   }
 }
