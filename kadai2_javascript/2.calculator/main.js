eec0bb644eb2a61106812c3d44409e0bcc242775
KAKI-EX <115675329+KAKI-EX@users.noreply.github.com>
2022-10-26 14:52:26 +0900

    .の挙動を修正

--- kadai2_javascript/2.calculator/main.js
+++ kadai2_javascript/2.calculator/main.js
@@ -1,1 +1,32 @@
 
+
+function buttonClick(target) {
+  let result = document.getElementById("result");
+  let target_value = target.innerHTML; 
+  let remove0 = result.innerHTML
+  let remove = remove0.slice(1);
+
+  if(target_value == "AC"){
+      result.innerHTML = "0";
+    }else if(target_value == "="){
+      booleanFlag = false
+      result.innerHTML = eval(result.innerHTML);
+
+    }else if(result.innerHTML == "0" && target_value !== "."){
+      result.innerHTML =target_value;
+    
+/*    }else if(target_value = "0"){
+      booleanFlag = true;
+      result.innerHTML =target_value;
+
+    }else if(result.innerHTML == "0" && booleanFlag == false){
+      console.log("成功")
+      result.innerHTML = remove + target_value;*/
+
+    }else{
+        result.innerHTML += target_value
+    }
+  }
+    
+
+
